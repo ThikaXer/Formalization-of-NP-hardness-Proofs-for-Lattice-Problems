a4182e2e65a3457835067aea6380a5905562ba53
Katharina Kreuzer <k.kreuzer@tum.de>
2021-10-27 16:15:29 +0200

    Bevor change representation of entries in vector. Try using a definit Basis instead of an ordering function.

--- CVP.thy
+++ CVP.thy
@@ -25,12 +25,20 @@ definition real_to_int_vec :: "real ^ 'n \<Rightarrow> int ^ ('n::finite)"  wher
   "real_to_int_vec v = vec_map floor v"
 
 
+
+definition is_lattice :: "('n::finite) lattice \<Rightarrow> bool" where
+  "is_lattice L \<equiv> (\<exists>B::(real ^'n ^'n). \<forall>v\<in>L. \<exists>z::int^'n. B *v (real_of_int_vec z) = v)"
+
+
+
 definition lin_combo :: "'a list \<Rightarrow> ('a, 'b) vec list \<Rightarrow> ('a::{plus, times, zero}, 'b::finite) vec" where
   "lin_combo zs vs = foldr (+) (map2 (*s) zs vs) (vec 0)"
 
-definition gen_lattice :: "(real, 'a) vec list \<Rightarrow> (real, 'a::finite) vec set" where
-  "gen_lattice vs = {v. \<exists>zs::int list. v = lin_combo zs vs}"
+definition gen_lattice_list :: "(real, 'a) vec list \<Rightarrow> (real, 'a::finite) vec set" where
+  "gen_lattice_list vs = {v. \<exists>zs::int list. v = lin_combo zs vs}"
 
+definition gen_lattice :: "(real^'a) ^'a \<Rightarrow> (real, 'a::finite) vec set" where
+  "gen_lattice vs = {v. \<exists>z::int ^'a. v = vs *v (real_of_int_vec z)}"
 
 
 text \<open>From now on work over fixed dimension!\<close>
@@ -58,27 +66,29 @@ lemma finv_f:
 unfolding finv_def using f_bij
 by (metis bij_betw_imp_inj_on comp_apply eq_id_iff the_inv_f_f)
 
+definition incr :: "'n \<Rightarrow> 'n" where
+  "incr i = finv (f i + 1)"
+
 text \<open>The CVP and SVP in $l_\infty$\<close>
 
 text \<open>The closest vector problem.\<close>
 definition is_closest_vec :: "'n lattice \<Rightarrow> real ^ 'n \<Rightarrow> real ^ 'n \<Rightarrow> bool" where
-  "is_closest_vec L b v \<equiv> (\<forall>x\<in>L. infnorm  (x - b) \<ge> 
-        infnorm (v - b) \<and> v\<in>L)"
+  "is_closest_vec L b v \<equiv> (is_lattice L) \<and> (\<forall>x\<in>L. infnorm  (x - b) \<ge>  infnorm (v - b) \<and> v\<in>L)"
 
 text \<open>The decision problem associated with solving CVP exactly.\<close>
 definition gap_cvp :: "('n lattice \<times> (real, 'n) vec \<times> real) set" where
-  "gap_cvp \<equiv> {(L, b, r). (\<exists>v\<in>L. infnorm (v - b) \<le> r)}"
+  "gap_cvp \<equiv> {(L, b, r). (is_lattice L) \<and> (\<exists>v\<in>L. infnorm (v - b) \<le> r)}"
 
 
 
 text \<open>The shortest vector problem.\<close>
 definition is_shortest_vec :: "'n lattice  \<Rightarrow> real ^ 'n \<Rightarrow> bool" where
-  "is_shortest_vec L v \<equiv> (\<forall>x\<in>L. infnorm (x) \<ge> infnorm (v) \<and> v\<in>L) "
+  "is_shortest_vec L v \<equiv> (is_lattice L) \<and> (\<forall>x\<in>L. infnorm (x) \<ge> infnorm (v) \<and> v\<in>L) "
 
 
 text \<open>The decision problem associated with solving SVP exactly.\<close>
 definition gap_svp :: "('n lattice \<times> real) set" where
-  "gap_svp \<equiv> {(L, r). (\<exists>v\<in>L. infnorm (v) \<le> r \<and> v\<noteq>0)}"
+  "gap_svp \<equiv> {(L, r). (is_lattice L) \<and> (\<exists>v\<in>L. infnorm (v) \<le> r \<and> v\<noteq>0)}"
 
 
 text \<open>Subset Sum Problem\<close>
@@ -90,13 +100,11 @@ definition subset_sum :: "((int ^'n) * int) set" where
 
 text \<open>Reduction function for cvp to subset sum\<close>
 
-definition gen_basis :: "('a::{zero,numeral}, 'n) vec \<Rightarrow> ('a, 'n) vec list" where
-  "gen_basis as =  map 
-    (\<lambda> j. (\<chi> i. if (f i) = 1 then as$(finv j) else (if (f i) = j + 1 then 2 else 0)))
-    [0..<n]"
+definition gen_basis :: "int ^ 'n \<Rightarrow> (real ^'n) ^'n" where
+  "gen_basis as = (\<chi> i j. if f i = 0 then as$j else (if f i = f j + 1 then 2 else 0))"
 
 
-definition gen_t :: "'a::one \<Rightarrow> ('a, 'n) vec" where
+definition gen_t :: "int \<Rightarrow> (real, 'n) vec" where
   "gen_t s = (\<chi> i. if f i = 0 then s else 1)"
 
 
@@ -104,19 +112,56 @@ definition gen_t :: "'a::one \<Rightarrow> ('a, 'n) vec" where
 definition reduce_cvp_subset_sum :: 
   "((int ^'n) * int) \<Rightarrow> (('n lattice) * (real ^'n) * real)" where
   "reduce_cvp_subset_sum input = 
-    (gen_lattice (gen_basis (real_of_int_vec (fst input))), gen_t (snd input), 1)"
+    (gen_lattice (gen_basis (fst input)), gen_t (snd input), 1)"
 
 text \<open>The Gap-CVP is NP-hard in l_infty.\<close>
 lemma "is_reduction reduce_cvp_subset_sum subset_sum gap_cvp"
 unfolding is_reduction_def
 proof (safe, goal_cases)
   case (1 as s)
-  then show ?case sorry
+  then obtain x where 
+    x_binary: "\<forall>i. x $ i \<in> {0, 1}" and 
+    x_lin_combo: "scalar_product x as = s" 
+    unfolding subset_sum_def by blast
+  define L where L_def: "L = fst (reduce_cvp_subset_sum (as, s))"
+  define b where b_def: "b = fst (snd (reduce_cvp_subset_sum (as, s)))"
+  define r where r_def: "r = snd (snd (reduce_cvp_subset_sum (as, s)))"
+  have "r = 1" using r_def reduce_cvp_subset_sum_def by auto
+  (*have "(L,b,r) = reduce_cvp_subset_sum (as, s)" using L_def b_def r_def by auto*)
+  define B where "B = gen_basis as"
+  have "B *v (real_of_int_vec x) - b = 
+    (\<chi> i. if f i = 0 then scalar_product x as - s else 2 * x$(incr i) - 1)"
+    (is "_ = ?goal_vec")
+    sorry
+  then have "infnorm (B *v (real_of_int_vec x) - b) = 
+    Max ({\<bar>scalar_product x as - s\<bar>} \<union> {\<bar>2*x$(finv i)-1\<bar> | i. i\<in>{1..<n} })"
+  proof -
+    have "infnorm (?goal_vec) = Max {\<bar>?goal_vec $(finv i)\<bar> | i. i\<in>{0..<n}}" 
+      apply (subst infnorm_Max) 
+
+find_theorems Basis
+
+    then have "\<dots> = Max ({\<bar>scalar_product x as - s\<bar>} \<union> {\<bar>2*x$(finv i)-1\<bar> | i. i\<in>{1..<n}})"
+      sorry
+    then show ?thesis sorry
+  qed
+  also have "\<dots> = Max ({0} \<union>  {y. \<exists>i\<in>{1..<n}. y = \<bar>2*x$(finv i)-1\<bar> })" 
+    using x_lin_combo by auto
+  also have "\<dots> \<le> Max ({0} \<union>  {1})"
+  proof -
+    have "\<bar>2*x$(finv i)-1\<bar> = 1" if "i\<in>{1..<n}" for i using x_binary
+      by (smt (z3) insert_iff singletonD)
+    then show ?thesis by auto
+  qed
+  also have "\<dots> \<le> r" using \<open>r=1\<close> by auto
+  finally have "infnorm (B *v (real_of_int_vec x) -b) \<le> r" by blast
+  have L_lattice: "is_lattice L" sorry
+  then have witness: "\<exists>v\<in>L. infnorm (v - b) \<le> r" sorry
+  show ?case unfolding gap_cvp_def using L_lattice witness L_def b_def r_def by force
 next
   case (2 as s)
   then show ?case sorry
 qed
-
 
 
 
