61a954832aa96591e06cb12d42c5ec69c5e46fcd
Katharina Kreuzer <k.kreuzer@tum.de>
2021-11-23 12:18:18 +0100

    finished proof is_indep_gen_svp_basis

--- SVP_vec.thy
+++ SVP_vec.thy
@@ -36,10 +36,57 @@ definition reduce_svp_bhle:: "(int vec \<times> real) \<Rightarrow> (lattice \<t
 
 text \<open>Lemmas for proof\<close>
 
-lemma is_indep_gen_svp_basis: 
-  "is_indep (gen_svp_basis a k)"
-unfolding is_indep_def gen_svp_basis_def apply auto
+lemma gen_svp_basis_mult: 
+  assumes "dim_vec z = dim_vec a + 1"
+  shows "(gen_svp_basis a k) *\<^sub>v z = vec (dim_vec a + 1) 
+         (\<lambda>i. if i < dim_vec a then z$i else (k+1) * (z \<bullet> real_of_int_vec a) + 
+              (2*(k+1)* (\<Sum> i \<in> {0 ..< dim_vec a}. a $ i) +1) * (z$(dim_vec a)))"
 sorry
+
+lemma is_indep_gen_svp_basis: 
+  assumes "k>0"
+  shows "is_indep (gen_svp_basis a k)"
+unfolding is_indep_def
+proof (safe, goal_cases)
+case (1 z)
+  have dim_row_dim_vec: "dim_row (gen_svp_basis a k) = dim_vec z" 
+    using 1(2) unfolding gen_svp_basis_def by auto
+  then have suc_dim_a_dim_z: "dim_vec z = dim_vec a + 1" unfolding gen_svp_basis_def by auto
+  have alt1: "(gen_svp_basis a k *\<^sub>v z) $ i = 0" if "i< dim_vec a +1"for i 
+    using 1(1) that unfolding gen_svp_basis_def by auto
+  have z_upto_last: "z$i = 0" if "i < dim_vec a" for i 
+  proof -
+    have elem: "((gen_svp_basis a k) *\<^sub>v z) $ i = z $ i" 
+      using gen_svp_basis_mult[OF suc_dim_a_dim_z] that by auto
+    show ?thesis by (subst elem[symmetric]) (use alt1[of i] that in \<open>auto\<close>) 
+  qed
+  moreover have "z $ (dim_vec a) = 0" 
+  proof -
+    have "0 = (gen_svp_basis a k *\<^sub>v z) $ (dim_vec a)" using alt1 by auto
+    also have "\<dots> = vec (Suc (dim_vec a))
+       (\<lambda>j. if j < dim_vec a then (k + 1) * real_of_int (a $ j)
+         else 2 * (k + 1) * real_of_int (sum (($) a) {0..<dim_vec a}) + 1) \<bullet> z" 
+      unfolding gen_svp_basis_def by auto
+    also have "\<dots> =  (2 * (k + 1) * real_of_int (sum (($) a) {0..<dim_vec a}) + 1 ) * (z$dim_vec a)"
+      unfolding scalar_prod_def using suc_dim_a_dim_z using z_upto_last by auto
+    finally have "0 = (2 * (k + 1) * real_of_int (sum (($) a) {0..<dim_vec a}) + 1 ) * 
+                      (z$dim_vec a)" by blast
+    moreover have "(2 * (k + 1) * real_of_int (sum (($) a) {0..<dim_vec a}) + 1 ) \<noteq> 0"
+    proof (safe)
+      assume ass: "(2 * (k + 1) * real_of_int (sum (($) a) {0..<dim_vec a}) + 1 ) = 0"
+      then show False
+        by (smt (z3) ass assms divide_less_eq_1_pos mult_minus_right nonzero_mult_div_cancel_left 
+          of_int_hom.hom_one of_int_less_iff of_int_minus)
+    qed
+    ultimately show ?thesis by auto
+  qed
+  ultimately have "z$i =  0" if "i < dim_vec z" for i using that suc_dim_a_dim_z
+    by (metis discrete le_eq_less_or_eq verit_comp_simplify1(3))
+  then show ?case by auto
+qed
+
+find_theorems "_$_ = _$_"
+
 
 text \<open>Well-definedness of reduction function\<close>
 
