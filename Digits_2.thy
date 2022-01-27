f35460a6148eb2489dd3e37a753ccdd6aac2d55c
Katharina Kreuzer <k.kreuzer@tum.de>
2022-01-27 13:37:20 +0100

    Sum equals zero if digits equals zero

--- Digits_2.thy
+++ Digits_2.thy
@@ -17,15 +17,19 @@ text \<open>Conversion from basis base to integers: \<open>from_digits n d\<clos
   output:& \<open>nat\<close>& natural number corresponding to $d(n-1) \dots d(0)$ as integer\\
 \end{tabular}
 \<close>
-fun from_digits :: "nat \<Rightarrow> (nat \<Rightarrow> int) \<Rightarrow> int" where
+fun from_digits :: "nat \<Rightarrow> (nat \<Rightarrow> nat) \<Rightarrow> nat" where
   "from_digits 0 d = 0"
-| "from_digits (Suc n) d = d 0 + base * from_digits n (d \<circ> Suc)"
+| "from_digits (Suc n) d = d 0 + nat base * from_digits n (d \<circ> Suc)"
 
 text \<open>Alternative definition using sum:\<close>
-lemma from_digits_altdef: "from_digits n d = (\<Sum>i<n. d i * base ^ i)"
+lemma from_digits_altdef: "from_digits n d = (\<Sum>i<n. d i * nat base ^ i)"
   by (induction n d rule: from_digits.induct)
      (auto simp add: sum.lessThan_Suc_shift o_def sum_distrib_left 
        sum_distrib_right mult_ac simp del: sum.lessThan_Suc)
+
+lemma int_from_digits:
+  "int (from_digits n d) = (\<Sum>i<n. int (d i) * base ^ i)"
+unfolding from_digits_altdef using base_pos by auto
 
 text \<open>Digit in basis base of some integer number: \<open>digit x i\<close>
 
@@ -36,13 +40,14 @@ text \<open>Digit in basis base of some integer number: \<open>digit x i\<close>
 \end{tabular}
 \<close>
 fun digit :: "int \<Rightarrow> nat \<Rightarrow> int" where
-  "digit x 0 = x mod base"
-| "digit x (Suc i) = digit (x div base) i"
+  "digit x 0 = \<bar>x\<bar> mod base"
+| "digit x (Suc i) = digit (\<bar>x\<bar> div base) i"
 
 text \<open>Alternative definition using divisor and modulo:\<close>
-lemma digit_altdef: "digit x i = (x div (base ^ i)) mod base"
+lemma digit_altdef: "digit x i = ( \<bar>x\<bar> div (base ^ i)) mod base"
   by (induction x i rule: digit.induct, simp)
-     (use base_pos zdiv_zmult2_eq in \<open>force\<close>) 
+     (smt (verit, best) Digits_2.digits.digit.simps(2) base_pos digits_axioms 
+      pos_imp_zdiv_neg_iff power_Suc zdiv_zmult2_eq zero_less_power)
 
 text \<open>Every digit must be smaller that the base.\<close>
 lemma digit_less_base: "digit x i < base"
@@ -50,76 +55,70 @@ lemma digit_less_base: "digit x i < base"
 
 text \<open>A representation in basis \<open>base\<close> of length $n$ must be less than $\<open>base\<close> ^ n$.\<close>
 lemma from_digits_less: 
-  assumes "\<forall>i<n. \<bar>d i\<bar> < base" 
+  assumes "\<forall>i<n. d i < base" 
   shows "from_digits n d < base ^ n"
 using assms proof (induct n d rule: from_digits.induct)
   case (2 n d)
   have "from_digits n (d \<circ> Suc) \<le> base ^ n -1" using 2 
     by (simp add: "2.hyps")
-  moreover have "\<bar>d 0\<bar> \<le> base -1" using 2 by simp
+  moreover have "d 0 \<le> base -1" using 2 by simp
   ultimately have "d 0 + base * from_digits n (d \<circ> Suc) \<le> 
       base - 1 + base * (base^(n) - 1)" 
     by (smt (verit, ccfv_SIG) base_pos mult_less_cancel_left_pos)
   then show "from_digits (Suc n) d < base ^ Suc n" 
-    using base_pos
-    by (simp add: right_diff_distrib)
+    using base_pos by (simp add: right_diff_distrib)
 qed auto
 
-(*
+
 text \<open>Lemmas for \<open>mod\<close> and \<open>div\<close> in number systems of basis \<open>base\<close>:\<close>
-lemma mod_base:  assumes "\<And>i. i<n \<Longrightarrow> \<bar>d i\<bar> < base" "n>0"
-  shows "from_digits n d mod base = d 0 " oops
+lemma mod_base:  assumes "\<And>i. i<n \<Longrightarrow> d i < base" "n>0"
+  shows "from_digits n d mod base = d 0 "
 proof -
-  have mod_fac: "(\<Sum>i<n. d i * base ^ i) mod base = 
+  have "(\<Sum>i<n. d i * base ^ i) mod base = 
           (\<Sum>i<n. d i * base ^ i mod base) mod base"  
   by (subst mod_sum_eq[symmetric]) simp
-  have sum_mod: "(\<Sum>i<n. d i * base ^ i mod base) =
-    d 0 * base ^ 0 mod base + (\<Sum>i<n - 1. d (Suc i) * base ^ Suc i mod base)"
-    using sum.lessThan_Suc_shift[of "(\<lambda>i. d i * base ^ i mod base)" "n-1"] assms
-    by simp
-  show ?thesis using assms(1)[OF assms(2)] 
-    unfolding from_digits_altdef mod_fac apply (subst sum_mod) apply simp
-using mod_less[of ]
-    apply (subst mod_less)
-    sledgehammer
+  then show ?thesis using assms 
+    sum.lessThan_Suc_shift[of "(\<lambda>i. d i * base ^ i mod base)" "n-1"]
+    int_from_digits by simp
 qed
-
-find_theorems "_ < _" "_ mod _ = (_::int)"
 
 lemma mod_base_i:  
-  assumes "\<And>i. i<n \<Longrightarrow> d i < base" "n>0" "i<n"
-  shows "(\<Sum>j=i..<n. d j * base ^ (j-i)) mod base = d i " oops
+  assumes "\<And>i. i<n \<Longrightarrow> (d i ::nat) < base" "n>0" "i<n"
+  shows "(\<Sum>j=i..<n. d j * base ^ (j-i)) mod base = d i "
 proof -
-  have "(\<Sum>j=i..<n. d j * base ^ (j-i)) mod base = 
+  have eq: "(\<Sum>j=i..<n. d j * base ^ (j-i)) mod base = 
         (\<Sum>j=i..<n. d j * base ^ (j-i) mod base) mod base"  
     by (subst mod_sum_eq[symmetric]) simp
-  then show ?thesis 
-    using assms split_sum_first_elt_less[where 
-        f = "(\<lambda>j. d j * base ^ (j-i) mod base)"] 
-    unfolding from_digits_altdef by simp
+  then show ?thesis  using assms 
+    split_sum_first_elt_less[where  f = "(\<lambda>j. d j * base ^ (j-i) mod base)"]
+    int_from_digits by auto
 qed
 
+
 lemma div_base_i: 
-  assumes "\<And>i. i<n \<Longrightarrow> d i < base" "n>0" "i<n"
+  assumes "\<And>i. i<n \<Longrightarrow> (d i::nat) < base" "n>0" "i<n"
   shows "from_digits n d div (base ^i) = (\<Sum>j=i..<n. d j * base ^ (j-i))"
-  unfolding from_digits_altdef proof -
+  unfolding int_from_digits proof -
   have base_exp: "base^(j) =  base^(j-i) * base^i" 
     if "j\<in>{i..<n}" for j 
     by (metis Nat.add_diff_assoc2 add_diff_cancel_right' atLeastLessThan_iff 
         power_add that)
   have first:"(\<Sum>j<i. d j * base ^ j)< base ^ i" 
     using assms from_digits_less[where n="i"] 
-    unfolding from_digits_altdef by auto
-  have "(\<Sum>j<n. d j * base ^ j) = 
+    unfolding int_from_digits by auto
+  have ge_0: "0 \<le> (\<Sum>j<i. int (d j) * base ^ j)" using base_pos
+  by (metis int_from_digits of_nat_0_le_iff)
+  have eq: "(\<Sum>j<n. d j * base ^ j) = 
           (\<Sum>j<i. d j * base ^ j) + (\<Sum>j=i..<n. d j * base ^ j)" 
     using assms split_sum_mid_less[where f="(\<lambda>j. d j * base^j)"] by auto
-  then have split_sum: "(\<Sum>j<n. d j * base ^ j) = 
+  have split_sum: "(\<Sum>j<n. d j * base ^ j) = 
       (\<Sum>j<i. d j * base ^ j) + base^i * (\<Sum>j=i..<n. d j * base ^ (j-i))"
-    using base_exp mult.assoc sum_distrib_right 
+    unfolding eq using base_exp mult.assoc sum_distrib_right
     by (smt (z3) mult.commute sum.cong)
-  then show "(\<Sum>i<n. d i * base ^ i) div base ^ i = 
-              (\<Sum>j = i..<n. d j * base ^ (j - i))" 
-    using first by (simp add:split_sum base_pos)
+  show "(\<Sum>i<n. d i * base ^ i) div base ^ i = 
+             (\<Sum>j = i..<n. d j * base ^ (j - i))" 
+    unfolding split_sum using base_pos first div_pos_pos_trivial[OF ge_0 first]
+    by (subst div_mult_self2, auto)
 qed
 
 
@@ -130,8 +129,8 @@ lemma digit_from_digits:
   shows   "digit (from_digits n d) i = d i"
   using assms proof (cases "i=0")
   case True
-  then show ?thesis 
-    by (simp add: assms(1) assms(2) digits.mod_base digits_axioms)
+  then show ?thesis
+  using assms(1) assms(3) mod_base by force
 next
   case False
   have "from_digits n d div base^i mod base = d i" 
@@ -153,22 +152,24 @@ proof -
         div_plus_div_distrib_dvd_left mod_mult_self3)
 qed
 
+(*
 lemma from_digits_digit:
-  assumes "x < base ^ n"
-  shows   "from_digits n (digit x) = x"
+  assumes "\<bar>x\<bar> < base ^ n"
+  shows   "from_digits n (\<lambda>i. nat \<bar>digit x i\<bar>) = \<bar>x::int\<bar>"
   using assms unfolding digit_altdef from_digits_altdef 
 proof (induction n arbitrary: x)
   case 0
   then show ?case by simp
 next
   case (Suc n)
-  define x_less where "x_less = x mod base^n"
-  define x_n where "x_n = x div base^n"
+  define x_less where "x_less = \<bar>x\<bar> mod base^n"
+  define x_n where "x_n = \<bar>x\<bar> div base^n"
   have "x_less < base^n" 
-    using x_less_def base_pos mod_less_divisor by presburger
+    using x_less_def base_pos mod_less_divisor[of "nat (base)^n" "nat \<bar>x\<bar>"]
+    by (metis Euclidean_Division.pos_mod_bound zero_less_power)
   then have IH_x_less:
     "(\<Sum>i<n. x_less div base ^ i mod base * base ^ i) = x_less" 
-    using Suc.IH by simp
+    using Suc.IH sorry by simp
   have "x_n < base" using \<open>x<base^Suc n\<close> 
     by auto (metis less_mult_imp_div_less x_n_def)
   then have "x_n mod base = x_n" by simp
@@ -195,7 +196,7 @@ qed
 
 text \<open>Stronger formulation of above lemma.\<close>
 lemma from_digits_digit': 
-  "from_digits n (digit x) = x mod (base ^ n)"
+  "from_digits n (\<lambda>i. nat \<bar>digit x i\<bar>) = \<bar>x\<bar> mod (base ^ n)"
   unfolding from_digits_altdef digit_altdef 
 proof (induction n arbitrary: x)
   case 0
@@ -233,12 +234,57 @@ next
 qed
 *)
 
-end
+
 lemma(in digits) digits_eq_0:
   assumes "x = 0"
   shows "digit x i = 0"
 by (simp add: assms digit_altdef)
+end
 
-find_theorems name: sign
+
+
+
+
+lemma split_digits_eq_zero:
+  assumes "a + base * b = 0" "\<bar>a\<bar><base" "(base::int)>2"
+  shows "a = 0 \<and> b=0"
+using assms proof (cases "b = 0")
+  case True
+  then show "a=0 \<and> b=0" using assms by auto
+next
+  case False
+  then have "\<bar>b\<bar> \<ge> 1" by auto
+  then have "\<bar>a\<bar> < \<bar>base * b\<bar>" using assms(2) assms(3)
+    by (subst abs_mult) (smt (verit) mult_le_cancel_left1)
+  moreover have "\<bar>a\<bar> = \<bar>base * b\<bar>" using assms(1) by auto
+  ultimately have False by auto
+  then show ?thesis by auto
+qed
+
+
+
+lemma respresentation_in_basis_eq_zero:
+  assumes "(\<Sum>i<n. c i * base^i) = 0" "(base::int) > 2" "\<And>i. i<n \<Longrightarrow> \<bar>c i\<bar> < base" "i<n"
+  shows "c i = 0"
+using assms proof (induction n arbitrary: i c)
+  case 0
+  then show ?case by auto
+next
+  case (Suc n)
+  have eq_0: "c 0 + base * (\<Sum>i<n. c (i+1) * base ^ i) = 0" 
+    using Suc(2) unfolding sum.lessThan_Suc_shift power_Suc sum.cong[of "{..<n}" "{..<n}" 
+      "(\<lambda>i. c (Suc i) * (base * base ^ i))" "(\<lambda>i. base * (c (n + 1) * base ^ n))"]
+    by (subst sum_distrib_left)(metis (no_types, lifting) Suc_eq_plus1 mult.left_commute 
+      mult_cancel_left1 power_0 sum.cong)
+  have "c 0 = 0 " and right: "(\<Sum>i<n. c (i+1) * base ^ i) = 0" 
+    using split_digits_eq_zero[OF eq_0 Suc(4)[of 0] Suc(3)] by auto
+  have lt_n: "c (i + 1) = 0" if "i<n" for i using Suc(4)
+    by (subst Suc(1)[OF right \<open>2<base\<close> _ \<open>i<n\<close>], auto) 
+  then show ?case
+  proof (cases "i=0")
+    case False
+    then show ?thesis using Suc(5) lt_n less_Suc_eq_0_disj by auto
+  qed (use \<open>c 0 = 0\<close> in \<open>auto\<close>)
+qed
 
 end
